424ff7bf98e1321b6e14bb7f2a895b58937a7b71
Coline Renuy <renuy.coline@gmail.com>
2022-10-06 15:07:08 +0200

    adding new page HomeTweets

---
+++ components/HomeTweets.js
@@ -0,0 +1,12 @@
+import styles from '../styles/HomeTweets.module.css';
+
+function HomeTweets() {
+
+    return(
+        <div>
+            <button className = {styles.button}>Sign In</button>
+        </div>
+    )
+}
+
+export default HomeTweets;
